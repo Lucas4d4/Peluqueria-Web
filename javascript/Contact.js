c57988adeea4f04eced747cca33998550f4a8e43
Lucas4d4 <101574260+Lucas4d4@users.noreply.github.com>
2026-06-15 16:16:02 -0300

    Se eliminó la función de botón de whatsapp
    Ya hay una función para la sección de Index.html

--- javascript/Contact.js
+++ javascript/Contact.js
@@ -17,57 +17,10 @@ form.addEventListener("submit", async (e) => {
   } else {
     status.textContent = "Error al enviar el mensaje.";
     status.style.color = "red";
-  }
+  } 
 });
+ 
 
-const contenedor=document.getElementById("mainContact");
-const boton=document.getElementById("btnWhatsapp");
-if(esMovil()){
-    const contenedorContact=document.getElementById("divContact");
-    
-    boton.style.display="none";
-    const nuevoBoton=document.createElement("button");
-    nuevoBoton.style.marginTop="40px";
-    nuevoBoton.style.marginBottom="40px";
-    nuevoBoton.value="Enviar por WhatsApp";
-    nuevoBoton.textContent="Enviar por WhatsApp";
-    nuevoBoton.style.fontSize="1.4rem";
-    nuevoBoton.style.padding="6px 8px 6px 8px";
-    nuevoBoton.style.borderRadius="2px";
-    nuevoBoton.style.borderStyle="solid";
-    nuevoBoton.style.borderColor="white";
-    nuevoBoton.style.borderWidth="1px";
-    nuevoBoton.style.backgroundColor="rgba(0, 180, 0, 1)";
-    nuevoBoton.style.color="white";
-    nuevoBoton.style.fontFamily="'Raleway',sans-serif";
-    contenedor.appendChild(nuevoBoton);
-}
-else{
-    const label=document.createElement("label");
-label.textContent="Entrar a WhatsApp";
-label.htmlFor = "nombreInput";
-label.id="lblWhatsapp";
-label.style.backgroundColor="rgba(0, 180, 0, 1)";
-label.style.display="none";
-label.style.position="absolute";
-label.style.padding="2px"; 
-label.style.borderRadius="3px";
-label.style.fontSize="1.2rem";
-
-contenedor.appendChild(label);//AGREGA AL DIV PADRE EL ELEMENTO CREADO
-
-boton.addEventListener("mouseenter", () =>{ 
-    const rect=boton.getBoundingClientRect();//getBoundingClientRect() obtiene la posición y el tamaño de un elemento en la ventana del navegador.
-    const contRect=contenedor.getBoundingClientRect()
-    label.style.left="78%";
-    label.style.top="83%";//usar un valor más estándar y agregarle animación
-    label.style.display="inline-block";
-});
-
-boton.addEventListener("mouseleave", () =>{
-    label.style.display="none";
-});    
-} 
 
 
 
